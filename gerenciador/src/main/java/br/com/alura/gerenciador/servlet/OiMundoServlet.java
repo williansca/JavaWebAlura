54da2e96170aa80df0291048e99e78ffa87fbdb2
User-1 <wscappinibritez@gmail.com>
2021-08-02 01:33:04 -0300

    Adding Alura's first Servlet Project

---
+++ gerenciador/src/main/java/br/com/alura/gerenciador/servlet/OiMundoServlet.java
@@ -0,0 +1,28 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+//nome do Servlet na URL que utilizaremos no navegador
+@WebServlet(urlPatterns = "/oi")
+public class OiMundoServlet extends HttpServlet {
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		PrintWriter out = resp.getWriter();
+        out.println("<html>");
+        out.println("<body>");
+        out.println("oi mundo, parabens vc escreveu o primeiro servlets.");
+        out.println("</body>");
+        out.println("</html>");
+        
+        System.out.println("o servlet OiMundoServlet foi chamado");
+	}
+
+}
